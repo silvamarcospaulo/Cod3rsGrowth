f977515e4651a2ade4a10dc05a8025a025277e50
Marcos Paulo Silva <smpmarcospaulo@icloud.com>
2024-05-15 08:31:15 -0300

    Implementação do modelo de dados

--- Cod3rsGrowth.Dominio/Card.cs
+++ Cod3rsGrowth.Dominio/Card.cs
@@ -15,18 +15,5 @@ namespace Cod3rsGrowth.Dominio
         public string cardRarity { get; }
         public decimal cardCost { get; set; }
         private List<PlayFormats> cardPlayFormat = new List<PlayFormats>();
-
-        public Card(int cardId, string cardName, string cardColor, string cardSet,
-            string cardRarity, decimal cardCost, List<PlayFormats> playFormats)
-        {
-            this.cardId = cardId;
-            this.cardName = cardName;
-            this.cardColor = cardColor;
-            this.cardSet = cardSet;
-            this.cardRarity = cardRarity;
-            this.cardCost = cardCost;
-            this.cardPlayFormat = playFormats;
-        }
-
     }
 }
