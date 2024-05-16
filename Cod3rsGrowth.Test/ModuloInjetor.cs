4109d076c0460d25867d182a05aa182c8b5d9f2d
Marcos Paulo Silva <smpmarcospaulo@icloud.com>
2024-05-16 12:34:15 -0300

    Configurando NuGet e criando o Módulo de Injeção

---
+++ Cod3rsGrowth.Test/ModuloInjetor.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.Test
+{
+    public class ModuloInjetor
+    {
+        static public void implementarServico(IServiceCollection serviceProvider)
+        {
+        }
+    }
+}
---
+++ Cod3rsGrowth.Test/TesteBase.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cod3rsGrowth.Test
+{
+    public class TesteBase : IDisposable
+    {
+        protected ServiceProvider ServiceProvider { get; set; }
+
+        public TesteBase()
+        {
+            var service = new ServiceCollection();
+            ModuloInjetor.implementarServico(service);
+            ServiceProvider = service.BuildServiceProvider();
+        }
+
+        public void Dispose()
+        {
+            ServiceProvider.Dispose();
+        }
+    }
+}
