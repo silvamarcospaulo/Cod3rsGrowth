9c6976333d10bf77e625cfc79a5070ff5371e71b
Marcos Paulo Silva <smpmarcospaulo@icloud.com>
2024-05-22 09:16:09 -0300

    Update: Regra de negocio ServicoJogador

--- Cod3rsGrowth.Servico/ServicoJogador/IServicoJogador.cs
+++ Cod3rsGrowth.Servico/ServicoJogador/IServicoJogador.cs
@@ -1,7 +1,13 @@
-﻿namespace Cod3rsGrowth.Dominio.Servicos
+﻿using Cod3rsGrowth.Dominio.Modelos;
+using Cod3rsGrowth.Dominio.Modelos.Enums;
+
+namespace Cod3rsGrowth.Dominio.Servicos
 {
     public interface IServicoJogador
     {
 
+        public decimal SomarPrecoDeTodasAsCartasDoJogador(Jogador jogador);
+
+        public int SomarQuantidadeDeBaralhosDoJogador(Jogador jogador);
     }
 }
--- Cod3rsGrowth.Servico/ServicoJogador/ServicoJogador.cs
+++ Cod3rsGrowth.Servico/ServicoJogador/ServicoJogador.cs
@@ -1,7 +1,18 @@
-﻿namespace Cod3rsGrowth.Dominio.Servicos
+﻿using Cod3rsGrowth.Dominio.Modelos;
+
+namespace Cod3rsGrowth.Dominio.Servicos
 {
     public class ServicoJogador : IServicoJogador
     {
+        public decimal SomarPrecoDeTodasAsCartasDoJogador(Jogador jogador)
+        {
+            return jogador.BaralhosJogador.SelectMany(baralhos => baralhos.CartasDoBaralho)
+                .Sum(carta => carta.QuantidadeCopiasDaCartaNoBaralho * carta.Carta.PrecoCarta);
+        }
 
+        public int SomarQuantidadeDeBaralhosDoJogador(Jogador jogador)
+        {
+            return jogador.QuantidadeDeBaralhosJogador;
+        }
     }
 }
