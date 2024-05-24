34ecf67d31c6d98735e360ff7aab58c7894c894b
Marcos Paulo Silva <smpmarcospaulo@icloud.com>
2024-05-24 13:43:09 -0300

    fix: ajustes Singleton

--- Cod3rsGrowth.Test/Singleton.cs
+++ Cod3rsGrowth.Test/Singleton.cs
@@ -5,22 +5,17 @@ public class Singleton
 {
     public sealed class SingletonTabelas
     {
-        private static  Lazy<SingletonTabelas>? Lazy = new Lazy<SingletonTabelas>(() => new SingletonTabelas());
-        public List<Carta> Cartas;
-        public List<CopiaDeCartasNoBaralho> CopiaDeCartasNosBaralhos;
-        public List<Baralho> Baralhos;
-        public List<Jogador> Jogadores;
+        private static readonly Lazy<List<Carta>> instanciaCartas = new Lazy<List<Carta>>(() => new List<Carta>());
+        private static readonly Lazy<List<CopiaDeCartasNoBaralho>> instanciaCopiaDeCartasNoBaralho = new Lazy<List<CopiaDeCartasNoBaralho>>(() => new List<CopiaDeCartasNoBaralho>());
+        private static readonly Lazy<List<Baralho>> instanciaBaralhos = new Lazy<List<Baralho>>(() => new List<Baralho>());
+        private static readonly Lazy<List<Jogador>> instanciaJogadores = new Lazy<List<Jogador>>(() => new List<Jogador>());
 
         private SingletonTabelas()
         {
-            Cartas = new List<Carta>();
-            CopiaDeCartasNosBaralhos = new List<CopiaDeCartasNoBaralho>();
-            Baralhos = new List<Baralho>();
-            Jogadores = new List<Jogador>();
         }
-        public static SingletonTabelas ObterInstancia
-        {
-            get{ return Lazy.Value; }
-        }
+        public static List<Carta> InstanciaCartas => instanciaCartas.Value;
+        public static List<CopiaDeCartasNoBaralho> InstanciaListasDeCartas => instanciaCopiaDeCartasNoBaralho.Value;
+        public static List<Baralho> InstanciaBaralhos => instanciaBaralhos.Value;
+        public static List<Jogador> InstanciaJogadores => instanciaJogadores.Value;
     }
 }
