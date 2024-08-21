59b5b6f7d6eb8c335952b4df6cfe9ad0206643d9
Marcos Paulo Silva <smpmarcospaulo@icloud.com>
2024-08-21 13:41:43 -0300

    (criar-projeto-ui5-basico-ajustando-a-configuracao-da-api) Ajustes configuração i18n e ajustes no esqueleto do projeto

--- Cod3rsGrowth.Web/wwwroot/app/App.controller.js
+++ Cod3rsGrowth.Web/wwwroot/app/App.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
 ], function (Controller) {
     "use strict";
 
-    return Controller.extend("ui5.mtgdeckbuilder.webapp.App", {
+    return Controller.extend("ui5.mtgdeckbuilder.app.App", {
 
         onChangeLanguage: function (oEvent){
             var selectedItem = oEvent.getParameter("selectedItem").getKey();
