0cf12264aae56e39eff86d941f5ef070725ea3ad
Marcos Paulo Silva <smpmarcospaulo@icloud.com>
2024-08-29 14:08:49 -0300

    (iimplementanto ...)

--- Cod3rsGrowth.Web/wwwroot/Component.js
+++ Cod3rsGrowth.Web/wwwroot/Component.js
@@ -1,17 +1,23 @@
 sap.ui.define([
-    "sap/ui/core/UIComponent"
-], (UIComponent) => {
+    "sap/ui/core/UIComponent",
+    "sap/ui/core/mvc/XMLView"
+], (UIComponent, XMLView) => {
     "use strict";
- 
+
     return UIComponent.extend("mtgdeckbuilder.Component", {
+
         metadata: {
             interfaces: ["sap.ui.core.IAsyncContentCreation"],
             manifest: "json"
         },
- 
-        init: function() {
+
+        init: function () {
             UIComponent.prototype.init.apply(this, arguments);
             this.getRouter().initialize();
+        },
+
+        getTable: function () {
+            return this.getRootControl().getContent()[0];
         }
     });
 });
